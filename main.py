22d2b2c75557932010cb693ac43f2940436c2dcc
d-komissarchik <0667669609@ukr.net>
2023-12-31 12:32:39 +0200

    main

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/hotels")
+async def get_hotels():
+    return {"Hotel 5 stars"}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
 
 
 #if __name__ == 'main':
