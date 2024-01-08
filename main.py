518a49ed924630bcf9cfc4979f6b48e995dd8565
d-komissarchik <0667669609@ukr.net>
2024-01-08 19:19:59 +0200

    added depends

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
@@ -6,23 +6,35 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
+class HotelsSearchArgs:
+    def __int__(
+            self,
+            location: str,
+            date_from: date,
+            date_to: date,
+            has_spa: Optional[bool] = None,
+            stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+# class SHotel(BaseModel):
+#     address: str
+#     name: str
+#     stars: int
 
 
 
 @app.get("/hotels")
 def get_hotels(
-    location: str,
-    date_from: date,
-    date_to: date,
-    has_spa: Optional[bool] = None,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-) -> list[SHotel]:
-    hotels = [{"address": "вул. Наукова, 1", "name": "Super Hotel", "stars": 5}]
-    return hotels
+    search_args: HotelsSearchArgs = Depends()
+):# -> list[SHotel]
+    #hotels = [{"address": "вул. Наукова, 1", "name": "Super Hotel", "stars": 5}]
+    return search_args
 
 
 class SBooking(BaseModel):
