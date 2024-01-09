4b2e6a5bf01f737892833d27b389573ce8f94015
d-komissarchik <0667669609@ukr.net>
2024-01-09 17:15:26 +0200

    added database

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+DB_HOST = "localhost"
+DB_PORT = 5432
+DB_USER = "postgres"
+DB_PASS = "postgres"
+DB_NAME = "postgres"
+
+DATABASE_URL = f"postgresql+asyncpg"
+
