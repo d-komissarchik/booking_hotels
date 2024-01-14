d3ca9d69356302f31dd4e65e33bd84dceb03d8d1
d-komissarchik <0667669609@ukr.net>
2024-01-14 14:50:59 +0200

    maked migrations

--- app/bookings/models.py
+++ app/bookings/models.py
@@ -4,9 +4,9 @@ from app.database import Base
 class Bookings(Base):
     __tablename__ = "bookings"
 
-    id = Column(Integer, primary_key=True)
-    room_id = Column(ForeignKey("rooms.id"))
-    user_id = Column(ForeignKey("users.id"))
+    id = Column(Integer, primary_key=True, nullable=False)
+    room_id = Column(ForeignKey("rooms.id"), nullable=False)
+    user_id = Column(ForeignKey("users.id"), nullable=False)
     date_from = Column(Date, nullable=False)
     date_to = Column(Date, nullable=False)
     price = Column(Integer, nullable=False)
