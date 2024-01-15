8d8c8ec5eadbc316603749b85fb3545b47d88fcd
d-komissarchik <0667669609@ukr.net>
2024-01-15 18:52:47 +0200

    added booking router

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,6 +1,19 @@
 from fastapi import APIRouter
 
+
 router = APIRouter(
     prefix="/bookings",
     tags=["Бронювання"],
 )
+
+
+@router.get("")
+def get_bookings():
+    pass
+
+
+
+@router.get("/booking_id")
+def get_bookings(booking_id):
+    pass
+
