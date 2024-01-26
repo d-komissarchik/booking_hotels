51f7eb066a83a4b9883451276a28aea641877d71
d-komissarchik <0667669609@ukr.net>
2024-01-26 13:26:05 +0200

    created authenticate user

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,6 +1,9 @@
 from jose import jwt
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from pydantic import EmailStr
+
+from app.users.dao import UsersDAO
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -27,4 +30,9 @@ def create_access_token(data: dict) -> str:
     return encoded_jwt
 
 
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user and verify_password(password, user.password):
+        return None
+    return user
 
