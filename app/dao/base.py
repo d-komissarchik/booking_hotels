2e0111bccc0101ec2cca6c79205d7e9fdd2cd9bc
d-komissarchik <0667669609@ukr.net>
2024-01-26 11:34:03 +0200

    fixed bugs

--- app/dao/base.py
+++ app/dao/base.py
@@ -31,4 +31,5 @@ class BaseDAO:
     async def add(cls, **data):
         async with async_session_maker() as session:
             query = insert(cls.model).values(**data)
+            await session.execute(query)
             await session.commit()
