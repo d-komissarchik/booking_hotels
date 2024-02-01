6fc099564eff61e66c98c6fc27c48a8ef2917f21
d-komissarchik <0667669609@ukr.net>
2024-02-01 14:40:36 +0200

    added get current user

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -7,13 +7,13 @@ from app.users.dao import UsersDAO
 
 
 def get_token(request: Request):
-    token = request.cookies.get("bookings_acces_token")
+    token = request.cookies.get("bookings_access_token")
     if not token:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return token
 
 
-async def get_current_user(token:str = Depends(get_token)):
+async def get_current_user(token: str = Depends(get_token)):
     try:
         payload = jwt.decode(
             token, info.secret_key, info.algorithm
@@ -21,13 +21,12 @@ async def get_current_user(token:str = Depends(get_token)):
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     expire: str = payload.get("exp")
-    if (not expire) or (int(expire) < datetime.utcnow()):
+    if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     user_id: str = payload.get("sub")
     if not user_id:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-    user = await UsersDAO.find_by_id(user_id)
+    user = await UsersDAO.find_by_id(int(user_id))
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
-
     return user
