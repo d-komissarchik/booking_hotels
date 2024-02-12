4b5c99a2708694587c1bd59539001d99a15ba7a1
d-komissarchik <0667669609@ukr.net>
2024-02-12 20:33:15 +0200

    added exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -10,3 +10,24 @@ IncorrectEmailOrPasswordException = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Пошта або пароль не вірний",
 )
+
+
+TokenExpireException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токен минув",
+)
+
+
+TokenAbsentException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Токен відсутній",
+)
+
+
+IncorrectTokenFormatException = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Неправильний формат токена",
+)
+
+
+UserIsNotPresentException = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
