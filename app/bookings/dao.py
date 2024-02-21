24d6478aca3c0c5b63176e22e90878e717fb923a
d-komissarchik <0667669609@ukr.net>
2024-02-21 13:32:32 +0200

    added sql request

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,3 +1,7 @@
+from datetime import date
+
+from sqlalchemy import delete, insert, select, and_, or_
+
 from app.dao.base import BaseDAO
 from app.bookings.models import Bookings
 
@@ -6,5 +10,17 @@ class BookingDAO(BaseDAO):
     model = Bookings
 
     @classmethod
-    async def add(cls):
-        pass
+    async def add(
+        cls,
+        room_id: int,
+        date_from: date,
+        date_to: date,
+        ):
+        booked_rooms = select(Bookings).where(
+            and_(
+                Bookings.room_id==1,
+                or_(
+
+                )
+            )
+        )
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import APIRouter, Request, Depends
 
 from app.bookings.dao import BookingDAO
@@ -16,8 +18,11 @@ async def get_bookings(user: Users = Depends(get_current_user)):
     return await BookingDAO.find_all(user_id=user.id)
 
 @router.post("")
-async def add_booking(user: Users = Depends(get_current_user)):
-    await BookingDAO.add()
+async def add_booking(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_current_user),
+):
+    await BookingDAO.add(room_id, date_from, date_to)
 
 
 
