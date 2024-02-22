0b561ecd416f627d75cd5798b3b7b1a0c01bbaa0
d-komissarchik <0667669609@ukr.net>
2024-02-22 12:54:52 +0200

    added sql request

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,9 +1,10 @@
 from datetime import date
 
-from sqlalchemy import delete, insert, select, and_, or_
+from sqlalchemy import delete, insert, select, and_, or_, func
 
 from app.dao.base import BaseDAO
 from app.bookings.models import Bookings
+from app.rooms.models import Rooms
 
 
 class BookingDAO(BaseDAO):
@@ -11,21 +12,27 @@ class BookingDAO(BaseDAO):
 
     @classmethod
     async def add(
-        cls,
-        room_id: int,
-        date_from: date,
-        date_to: date,
-        ):
+            cls,
+            room_id: int,
+            date_from: date,
+            date_to: date,
+    ):
         booked_rooms = select(Bookings).where(
             and_(
-                Bookings.room_id==1,
+                Bookings.room_id == 1,
                 or_(
                     and_(
                         Bookings.date_from >= date_from,
                         Bookings.date_from <= date_to
                     ),
-
+                    and_(
+                        Bookings.date_from <= date_from,
+                        Bookings.date_to > date_from
                     ),
                 )
             )
-        )
+        ).cte("booked_rooms")
+
+        rooms_left = select(
+            Rooms.quantity - func.count(booked_rooms.room_id)
+                            ).select_from(Rooms)
